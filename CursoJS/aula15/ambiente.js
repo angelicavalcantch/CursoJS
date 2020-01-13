cdf56b9b5cd0f52ee65baf10f47ea162b7718222
Angélica Cavalcante <43219714+angelicavalcantch@users.noreply.github.com>
2020-01-13 17:05:15 -0300

    Array - example

--- CursoJS/aula15/ambiente.js
+++ CursoJS/aula15/ambiente.js
@@ -1,6 +1,8 @@
-let num = [ 5, 8, 2, 9, 3]
+/* let num = [ 5, 8, 2, 9, 3]
 num [4] = 6
 num.push(7)
 
 console.log(`A organização dos números é ${num.sort()}`)
-
+*/
+let amor = ['love', 'amour', 'amore']
+console.log(`Amor in English is "${amor[0]}", in French is "${amor[1]}", and in Italian is "${amor[2]}".`)
